600c56bb77372c2ffe832cd0168a2f0f5d8677dc
japstoor <japs.009@gmail.com>
2020-09-04 09:22:28 +0530

    Team Member

--- js/script.js
+++ js/script.js
@@ -2,3 +2,77 @@ $(window).on('load', function() {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+/*-----------Team--------*/
+$(function(){
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"</i>', '<i class="fa fa-angle-right"</i>' ]
+    });
+});
+
+/*------------Progress bars------------------*/
+$(function(){
+    
+    $("#progress-elements").waypoint(function(){
+        
+        $(".progress-bar").each(function(){
+            $(this).animate({width: $(this).attr("aria-valuenow") + "%"
+        }, 1000);
+        });
+        this.distroy();
+    }, {offset: 'bottom-in-view'});
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
