1aa4e15af03a9a78dffd9fb5484462c18493e97b
japstoor <japs.009@gmail.com>
2020-09-08 23:28:59 +0530

    Testimonial Section

--- js/script.js
+++ js/script.js
@@ -65,8 +65,19 @@ $(function (){
       });
 });
 
-
-
+/*---------------Testimonials----------------*/
+$(function(){
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"</i>', '<i class="fa fa-angle-right"</i>' ]
+    });
+});
 
 
 
