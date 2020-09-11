fb0a838f1c44417c855019ca75c6b580226be525
japstoor <japs.009@gmail.com>
2020-09-11 13:09:46 +0530

    Clients Section

--- js/script.js
+++ js/script.js
@@ -87,6 +87,19 @@ $(function(){
     });
 });
 
+/*-----------------clients--------------------------*/
+$(function(){
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"</i>', '<i class="fa fa-angle-right"</i>' ]
+    });
+});
 
 
 
